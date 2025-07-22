da95b53fa22d784b2c400a5f3853c368fd7ba2d4
tldus2355 <tldus2355@gmail.com>
2025-07-23 00:25:52 +0900

    Tiled setup
    Tiled setup

---
+++ Tiled/ElectroTileSet.tsx
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="ElectroTileSet" tilewidth="32" tileheight="32" tilecount="8" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="8">
+  <image source="TiledsetImg/Start.png" width="32" height="32"/>
+ </tile>
+ <tile id="9">
+  <image source="TiledsetImg/Enemy.png" width="32" height="32"/>
+ </tile>
+ <tile id="10">
+  <image source="TiledsetImg/-.png" width="32" height="32"/>
+ </tile>
+ <tile id="11">
+  <image source="TiledsetImg/ㄴ.png" width="32" height="32"/>
+ </tile>
+ <tile id="12">
+  <image source="TiledsetImg/ㅗ.png" width="32" height="32"/>
+ </tile>
+ <tile id="13">
+  <image source="TiledsetImg/+.png" width="32" height="32"/>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="voltage" type="int" value="5"/>
+  </properties>
+  <image source="TiledsetImg/Battery.png" width="32" height="32"/>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="resistance" type="int" value="1"/>
+  </properties>
+  <image source="TiledsetImg/Resist.png" width="32" height="32"/>
+ </tile>
+</tileset>
