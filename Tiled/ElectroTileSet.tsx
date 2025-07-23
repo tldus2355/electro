8f3130758a0a52174e52dd386acc985640a526e9
tldus2355 <tldus2355@gmail.com>
2025-07-23 13:50:46 +0900

    tiled setup2
    tiled setup2

--- Tiled/ElectroTileSet.tsx
+++ Tiled/ElectroTileSet.tsx
@@ -1,10 +1,16 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="ElectroTileSet" tilewidth="32" tileheight="32" tilecount="8" columns="0">
+<tileset version="1.10" tiledversion="1.11.2" name="ElectroTileSet" tilewidth="32" tileheight="32" tilecount="9" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="8">
-  <image source="TiledsetImg/Start.png" width="32" height="32"/>
+  <properties>
+   <property name="voltage" type="int" value="5"/>
+  </properties>
+  <image source="TiledsetImg/Player.png" width="32" height="32"/>
  </tile>
  <tile id="9">
+  <properties>
+   <property name="voltage" type="int" value="5"/>
+  </properties>
   <image source="TiledsetImg/Enemy.png" width="32" height="32"/>
  </tile>
  <tile id="10">
@@ -19,13 +25,16 @@
  <tile id="13">
   <image source="TiledsetImg/+.png" width="32" height="32"/>
  </tile>
- <tile id="14">
+ <tile id="16">
+  <image source="TiledsetImg/half.png" width="32" height="32"/>
+ </tile>
+ <tile id="17">
   <properties>
-   <property name="voltage" type="int" value="5"/>
+   <property name="voltage" type="int" value="3"/>
   </properties>
   <image source="TiledsetImg/Battery.png" width="32" height="32"/>
  </tile>
- <tile id="15">
+ <tile id="18">
   <properties>
    <property name="resistance" type="int" value="1"/>
   </properties>
