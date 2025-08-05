24447728c35ac18569ea165602216ff8bcf107af
tldus2355 <tldus2355@gmail.com>
2025-08-05 13:29:08 +0900

    타일추가
    여러 타일을 추가함

--- Tiled/ElectroTileSet.tsx
+++ Tiled/ElectroTileSet.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="ElectroTileSet" tilewidth="32" tileheight="32" tilecount="9" columns="0">
+<tileset version="1.10" tiledversion="1.11.2" name="ElectroTileSet" tilewidth="32" tileheight="32" tilecount="14" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="8">
   <properties>
@@ -39,5 +39,20 @@
    <property name="resistance" type="int" value="1"/>
   </properties>
   <image source="TiledsetImg/Resist.png" width="32" height="32"/>
+ </tile>
+ <tile id="19">
+  <image source="TiledsetImg/Capacitor.png" width="32" height="32"/>
+ </tile>
+ <tile id="20">
+  <image source="TiledsetImg/Diode.png" width="32" height="32"/>
+ </tile>
+ <tile id="21">
+  <image source="TiledsetImg/Fuse.png" width="32" height="32"/>
+ </tile>
+ <tile id="22">
+  <image source="TiledsetImg/Inductor.png" width="32" height="32"/>
+ </tile>
+ <tile id="23">
+  <image source="TiledsetImg/Semi_conductor.png" width="32" height="32"/>
  </tile>
 </tileset>
